d00bcbca0196bec89ca03b7195bff9e73286dba4
sunya <mugen.Xyz@gmail.com>
2015-08-16 22:55:14 +0900

    一通り実装した。

--- app/src/main/java/net/unsweets/kotoris/Welcome.java
+++ app/src/main/java/net/unsweets/kotoris/Welcome.java
@@ -1,18 +1,30 @@
 package net.unsweets.kotoris;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class Welcome extends ActionBarActivity {
+public class Welcome extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-
+        Button addAccountButton = (Button) findViewById(R.id.welcome_add_account_button);
+        addAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
 }
