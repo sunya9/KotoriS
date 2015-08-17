b83023193fa6b937993d2b1dab38cdc38dbaaaeb
sunya <mugen.Xyz@gmail.com>
2015-08-18 03:58:12 +0900

    setRetainInstanceをtrueにした。

--- app/src/main/java/net/unsweets/kotoris/fragments/CacheFragment.java
+++ app/src/main/java/net/unsweets/kotoris/fragments/CacheFragment.java
@@ -1,8 +1,19 @@
 package net.unsweets.kotoris.fragments;
 
+import android.app.Fragment;
+import android.os.Bundle;
+
 /**
  * Created by mugen_000 on 2015/08/18.
  */
-public class CacheFragment {
+public class CacheFragment extends Fragment{
     private static final String TAG = CacheFragment.class.getSimpleName();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    
 }
