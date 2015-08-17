06cac1ff0f74e62f90b31c3e7b9b4f5683c926d6
sunya <mugen.Xyz@gmail.com>
2015-08-17 15:44:53 +0900

    継承元をAppCompatActivityに変更。

--- app/src/main/java/net/unsweets/kotoris/BaseActivity.java
+++ app/src/main/java/net/unsweets/kotoris/BaseActivity.java
@@ -2,10 +2,11 @@ package net.unsweets.kotoris;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class BaseActivity extends ActionBarActivity {
+public class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
