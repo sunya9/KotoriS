9a6f0b9588673b4bdd8316bb8c697b7e73a12ec1
sunya <mugen.Xyz@gmail.com>
2015-08-18 03:56:47 +0900

    CacheFragmentを追加

---
+++ app/src/main/java/net/unsweets/kotoris/fragments/CacheFragment.java
@@ -0,0 +1,8 @@
+package net.unsweets.kotoris.fragments;
+
+/**
+ * Created by mugen_000 on 2015/08/18.
+ */
+public class CacheFragment {
+    private static final String TAG = CacheFragment.class.getSimpleName();
+}
