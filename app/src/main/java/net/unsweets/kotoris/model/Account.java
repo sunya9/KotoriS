c23c4992fd7591b1e56c78c6d4296290df47ed78
sunya <mugen.Xyz@gmail.com>
2015-08-17 16:08:31 +0900

    Accountモデルの定義を追加。

--- app/src/main/java/net/unsweets/kotoris/model/Account.java
+++ app/src/main/java/net/unsweets/kotoris/model/Account.java
@@ -1,8 +1,32 @@
 package net.unsweets.kotoris.model;
 
+import android.provider.BaseColumns;
+
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+
 /**
  * Created by mugen_000 on 2015/08/17.
  */
-public class Account {
+
+@Table(name = "accounts", id = BaseColumns._ID)
+public class Account extends Model {
     private static final String TAG = Account.class.getSimpleName();
+
+    @Column(name = "user_id")
+    public String userId;
+
+    @Column(name = "screen_name")
+    public String screenName;
+
+    @Column(name = "handle_name")
+    public String handleName;
+
+    @Column(name = "rank")
+    public int rank;
+
+    public Account(){
+        super();
+    }
 }
