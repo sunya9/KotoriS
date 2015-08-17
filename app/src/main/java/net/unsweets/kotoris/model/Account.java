05464b427afce3868b9a5b347304627813e18f9e
sunya <mugen.Xyz@gmail.com>
2015-08-17 16:07:59 +0900

    Accountモデルを追加。

---
+++ app/src/main/java/net/unsweets/kotoris/model/Account.java
@@ -0,0 +1,8 @@
+package net.unsweets.kotoris.model;
+
+/**
+ * Created by mugen_000 on 2015/08/17.
+ */
+public class Account {
+    private static final String TAG = Account.class.getSimpleName();
+}
