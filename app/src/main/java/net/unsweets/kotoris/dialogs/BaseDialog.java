03fc29ebab4340451a3fdb3bd344a9c5a9b63aac
sunya <mugen.Xyz@gmail.com>
2015-08-20 10:37:06 +0900

    ダイアログ基底クラスを追加。

---
+++ app/src/main/java/net/unsweets/kotoris/dialogs/BaseDialog.java
@@ -0,0 +1,8 @@
+package net.unsweets.kotoris.dialogs;
+
+/**
+ * Created by mugen_000 on 2015/08/20.
+ */
+public class BaseDialog {
+    private static final String TAG = BaseDialog.class.getSimpleName();
+}
