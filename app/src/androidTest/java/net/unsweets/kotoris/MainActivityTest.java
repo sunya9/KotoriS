4ab616255be652f5e9750c309b2bce084e07f342
sunya <mugen.Xyz@gmail.com>
2015-08-21 20:59:30 +0900

    MainActivityTestのひな形を追加。

---
+++ app/src/androidTest/java/net/unsweets/kotoris/MainActivityTest.java
@@ -0,0 +1,28 @@
+package net.unsweets.kotoris;
+
+import android.test.ActivityUnitTestCase;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by mugen_000 on 2015/08/21.
+ */
+public class MainActivityTest extends ActivityUnitTestCase<MainActivity> {
+
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testOnCreate() throws Exception {
+
+    }
+}
