5440a7f5fc54c0441e358d249f10d06816207a52
sunya <mugen.Xyz@gmail.com>
2015-08-22 05:35:33 +0900

    LoginActivityが表示されるかどうかのテストを作成。

--- app/src/androidTest/java/net/unsweets/kotoris/WelcomeTest.java
+++ app/src/androidTest/java/net/unsweets/kotoris/WelcomeTest.java
@@ -1,22 +1,38 @@
 package net.unsweets.kotoris;
 
-import junit.framework.TestCase;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.internal.view.ContextThemeWrapper;
+import android.test.ActivityUnitTestCase;
+import android.test.suitebuilder.annotation.MediumTest;
+import android.widget.Button;
 
 /**
  * Created by mugen_000 on 2015/08/21.
  */
-public class WelcomeTest extends TestCase {
+public class WelcomeTest extends BaseActivityUnitTestCase<Welcome> {
 
+    private Activity mActivity;
+
+    public WelcomeTest() {
+        super(Welcome.class);
+    }
+
+
+    @Override
     public void setUp() throws Exception {
         super.setUp();
-
+        Intent intent = new Intent(getInstrumentation()
+                .getTargetContext(), Welcome.class);
+        mActivity = startActivity(intent, null, null);
     }
 
-    public void tearDown() throws Exception {
-
-    }
-
-    public void testOnCreate() throws Exception {
-
+    public void testLaunchLoginActivityAndFinishedSelf() {
+        Button addAccountButton = (Button) mActivity.findViewById(R.id.welcome_add_account_button);
+        addAccountButton.performClick();
+        assertFalse("Welcome screen is not finished.",isFinishCalled());
+        Intent loginIntent = getStartedActivityIntent();
+        assertNotNull("Start Login Activity.", loginIntent);
     }
 }
