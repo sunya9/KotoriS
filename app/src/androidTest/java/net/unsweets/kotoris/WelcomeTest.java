402ee32c0e2eaab3c5e244b9f15fc6cfa8957e3b
sunya <mugen.Xyz@gmail.com>
2015-08-21 21:00:42 +0900

    WelcomeActivityのテストケースのひな形を追加。

---
+++ app/src/androidTest/java/net/unsweets/kotoris/WelcomeTest.java
@@ -0,0 +1,22 @@
+package net.unsweets.kotoris;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by mugen_000 on 2015/08/21.
+ */
+public class WelcomeTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testOnCreate() throws Exception {
+
+    }
+}
