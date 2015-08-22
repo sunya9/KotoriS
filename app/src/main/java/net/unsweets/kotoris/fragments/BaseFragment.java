309c84696453520a458b2b68f02f4a00c1b7c762
sunya <mugen.Xyz@gmail.com>
2015-08-22 19:32:15 +0900

    回転時などに再生成されないようにした。

--- app/src/main/java/net/unsweets/kotoris/fragments/BaseFragment.java
+++ app/src/main/java/net/unsweets/kotoris/fragments/BaseFragment.java
@@ -1,8 +1,17 @@
 package net.unsweets.kotoris.fragments;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
 /**
  * Created by mugen_000 on 2015/08/22.
  */
-public class BaseFragment {
+public class BaseFragment extends Fragment{
     private static final String TAG = BaseFragment.class.getSimpleName();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
 }
