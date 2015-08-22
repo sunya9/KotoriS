c5c1d20c14c22b2c72515723044dcc549fed10b3
sunya <mugen.Xyz@gmail.com>
2015-08-22 20:59:27 +0900

    LoginFragmentをLoginのインナークラスにした。

--- app/src/main/java/net/unsweets/kotoris/fragments/LoginFragment.java
+++
@@ -1,8 +0,0 @@
-package net.unsweets.kotoris.fragments;
-
-/**
- * Created by mugen_000 on 2015/08/22.
- */
-public class LoginFragment {
-    private static final String TAG = LoginFragment.class.getSimpleName();
-}
