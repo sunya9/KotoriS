a561e3fb5c771c9e8970bee86019d341eed1de88
sunya <mugen.Xyz@gmail.com>
2015-08-22 15:44:54 +0900

    ProgressDialog作成。

---
+++ app/src/main/java/net/unsweets/kotoris/dialogs/ProgressDialog.java
@@ -0,0 +1,8 @@
+package net.unsweets.kotoris.dialogs;
+
+/**
+ * Created by mugen_000 on 2015/08/22.
+ */
+public class ProgressDialog {
+    private static final String TAG = ProgressDialog.class.getSimpleName();
+}
