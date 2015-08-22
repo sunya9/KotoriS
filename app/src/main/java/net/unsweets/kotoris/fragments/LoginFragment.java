63dc31a30dcc5d696f11aaa481af42d2310b1595
sunya <mugen.Xyz@gmail.com>
2015-08-22 19:18:37 +0900

    ログインフラグメントを追加。

---
+++ app/src/main/java/net/unsweets/kotoris/fragments/LoginFragment.java
@@ -0,0 +1,8 @@
+package net.unsweets.kotoris.fragments;
+
+/**
+ * Created by mugen_000 on 2015/08/22.
+ */
+public class LoginFragment {
+    private static final String TAG = LoginFragment.class.getSimpleName();
+}
