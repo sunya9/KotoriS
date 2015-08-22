232ae96bd5c66424625c6131af4b0336a449a987
sunya <mugen.Xyz@gmail.com>
2015-08-22 19:31:33 +0900

    Fragmentの基底クラスを追加。

---
+++ app/src/main/java/net/unsweets/kotoris/fragments/BaseFragment.java
@@ -0,0 +1,8 @@
+package net.unsweets.kotoris.fragments;
+
+/**
+ * Created by mugen_000 on 2015/08/22.
+ */
+public class BaseFragment {
+    private static final String TAG = BaseFragment.class.getSimpleName();
+}
