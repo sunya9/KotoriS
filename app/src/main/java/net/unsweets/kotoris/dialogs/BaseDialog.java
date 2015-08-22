dbf23b6935b72f62a7aae211c825e44439195860
sunya <mugen.Xyz@gmail.com>
2015-08-22 15:44:42 +0900

    ダイアログ基底クラスのひな形を作成。

--- app/src/main/java/net/unsweets/kotoris/dialogs/BaseDialog.java
+++ app/src/main/java/net/unsweets/kotoris/dialogs/BaseDialog.java
@@ -1,8 +1,22 @@
 package net.unsweets.kotoris.dialogs;
 
+import android.app.DialogFragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatDialog;
+
 /**
  * Created by mugen_000 on 2015/08/20.
  */
-public class BaseDialog {
+abstract public class BaseDialog extends DialogFragment{
     private static final String TAG = BaseDialog.class.getSimpleName();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    abstract public void show();
+    abstract public BaseDialog newInstance();
 }
