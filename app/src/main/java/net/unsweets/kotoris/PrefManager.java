2ff9787e4d3593fce4afb4acd00338f17ff8048f
sunya <mugen.Xyz@gmail.com>
2015-08-23 02:08:09 +0900

    設定を管理するクラスを追加。

---
+++ app/src/main/java/net/unsweets/kotoris/PrefManager.java
@@ -0,0 +1,8 @@
+package net.unsweets.kotoris;
+
+/**
+ * Created by mugen_000 on 2015/08/23.
+ */
+public class PrefManager {
+    private static final String TAG = PrefManager.class.getSimpleName();
+}
