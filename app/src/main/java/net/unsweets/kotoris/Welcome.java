d15e99efc9346d8df7f1ef6e740c61b969a4eec8
sunya <mugen.Xyz@gmail.com>
2015-08-23 21:00:13 +0900

    アカウント作成成功時にMainActivityに戻すようにした。

--- app/src/main/java/net/unsweets/kotoris/Welcome.java
+++ app/src/main/java/net/unsweets/kotoris/Welcome.java
@@ -1,15 +1,12 @@
 package net.unsweets.kotoris;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class Welcome extends AppCompatActivity {
+public class Welcome extends BaseActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +17,31 @@ public class Welcome extends AppCompatActivity {
         addAccountButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Login.class);
-                startActivity(intent);
+                Intent intent = Login.newIntent(getApplicationContext());
+                startActivityForResult(intent, REQUEST_CODE.FIRST_TIME.ordinal());
             }
         });
     }
 
+    ;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            switch (REQUEST_CODE.values()[requestCode]) {
+                case FIRST_TIME: {
+                    PrefManager instance = PrefManager.getInstance(this);
+                    instance.setExistsAccount(true);
+                    setResult(RESULT_OK);
+                    finish();
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    enum REQUEST_CODE {
+        FIRST_TIME
+    }
 }
