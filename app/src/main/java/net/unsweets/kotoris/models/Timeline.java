8328ddf807ce5592c508e9aceb0d1576a2f41c62
sunya <mugen.Xyz@gmail.com>
2015-08-23 20:17:42 +0900

    ColumnモデルをTimelineモデルに名称変更。

---
+++ app/src/main/java/net/unsweets/kotoris/models/Timeline.java
@@ -0,0 +1,14 @@
+package net.unsweets.kotoris.models;
+
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Table;
+
+/**
+ * Created by mugen_000 on 2015/08/23.
+ */
+@Table(name = "columns")
+public class Timeline extends Model {
+    private static final String TAG = Timeline.class.getSimpleName();
+    
+    @
+}
