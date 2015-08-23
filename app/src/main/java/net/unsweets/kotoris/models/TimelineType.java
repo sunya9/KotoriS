a35bbeae3f00bc4b8021b9a449207263f1b3df56
sunya <mugen.Xyz@gmail.com>
2015-08-23 20:18:03 +0900

    タイムラインのタイプを定義するモデルを追加。

---
+++ app/src/main/java/net/unsweets/kotoris/models/TimelineType.java
@@ -0,0 +1,20 @@
+package net.unsweets.kotoris.models;
+
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+
+/**
+ * Created by mugen_000 on 2015/08/23.
+ */
+
+@Table(name = "timeline_type")
+public class TimelineType  extends Model{
+    private static final String TAG = TimelineType.class.getSimpleName();
+
+    @Column(name = "type")
+    public int type;
+
+    @Column(name = "name")
+    public String name;
+}
