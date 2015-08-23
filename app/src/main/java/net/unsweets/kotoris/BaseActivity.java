d670344705e93b124aa480d9f4619254920c600e
sunya <mugen.Xyz@gmail.com>
2015-08-23 21:01:32 +0900

    android.R.id.homeをおした時のデフォルトの動作を追加。

--- app/src/main/java/net/unsweets/kotoris/BaseActivity.java
+++ app/src/main/java/net/unsweets/kotoris/BaseActivity.java
@@ -1,9 +1,7 @@
 package net.unsweets.kotoris;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
 import android.view.MenuItem;
 
 public class BaseActivity extends AppCompatActivity {
@@ -11,5 +9,18 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case android.R.id.home: {
+                finish();
+                return true;
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
