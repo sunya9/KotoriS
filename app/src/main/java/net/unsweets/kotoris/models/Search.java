9e6580a0a7eb14394cc16d09a81b610446e252ff
sunya <mugen.Xyz@gmail.com>
2015-08-23 20:24:58 +0900

    検索情報を提供するSearchモデルを追加。

---
+++ app/src/main/java/net/unsweets/kotoris/models/Search.java
@@ -0,0 +1,8 @@
+package net.unsweets.kotoris.models;
+
+/**
+ * Created by mugen_000 on 2015/08/23.
+ */
+public class Search {
+    private static final String TAG = Search.class.getSimpleName();
+}
